8c4cf34df90a5719b239ff302838caed3636f3dc
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-22 16:57:18 +0100

    fixes

--- src/main/java/ygorgarofalo/SpringU2W3D1/controllers/AuthController.java
+++ src/main/java/ygorgarofalo/SpringU2W3D1/controllers/AuthController.java
@@ -2,12 +2,16 @@ package ygorgarofalo.SpringU2W3D1.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ygorgarofalo.SpringU2W3D1.entities.User;
+import ygorgarofalo.SpringU2W3D1.exceptions.BadRequestExc;
 import ygorgarofalo.SpringU2W3D1.payloads.UserLoginDTO;
 import ygorgarofalo.SpringU2W3D1.payloads.UserLoginResponseDTO;
+import ygorgarofalo.SpringU2W3D1.payloads.UserPayloadDTO;
+import ygorgarofalo.SpringU2W3D1.payloads.UserResponseDTO;
 import ygorgarofalo.SpringU2W3D1.services.AuthService;
 import ygorgarofalo.SpringU2W3D1.services.UserService;
 
@@ -25,10 +29,24 @@ public class AuthController {
 
     @PostMapping("/login")
     public UserLoginResponseDTO login(@RequestBody UserLoginDTO payload) {
-        
+
 
         String accessToken = authService.authUser(payload);
         return new UserLoginResponseDTO(accessToken);
     }
 
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO createUser(@RequestBody @Validated UserPayloadDTO newUserPayload, BindingResult validation) {
+        // Per completare la validazione devo in qualche maniera fare un controllo del tipo: se ci sono errori -> manda risposta con 400 Bad Request
+        System.out.println(validation);
+        if (validation.hasErrors()) {
+            System.out.println(validation.getAllErrors());
+            throw new BadRequestExc("Ci sono errori nel payload!"); // L'eccezione arriverà agli error handlers tra i quali c'è quello che manderà la risposta con status code 400
+        } else {
+            User newUser = userService.saveUser(newUserPayload);
+
+            return new UserResponseDTO(newUser.getId());
+        }
+    }
 }
